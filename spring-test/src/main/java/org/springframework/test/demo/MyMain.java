569a78e3f8c6579b411b0064bd1c66758b79606b
xulong.song <xulong.song@chintanneng.com>
2022-11-22 10:31:45 +0800

    test main class

---
+++ spring-test/src/main/java/org/springframework/test/demo/MyMain.java
@@ -0,0 +1,11 @@
+package org.springframework.test.demo;
+
+/**
+ * @author xulong.song
+ * @date 2022/11/22 10:30
+ */
+public class MyMain {
+	public static void main(String[] args) {
+		System.out.println(123);
+	}
+}
